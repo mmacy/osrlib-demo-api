c270d2ba4d65dbf8596dffbf185c092229779743
Marsh Macy <mmacy@users.noreply.github.com>
2022-12-22 23:06:21 -0800

    Initial project add

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# osrlib-demo-api
-Sample implementation of OSRlib.NET in an ASP.NET web API with controllers.
+# Demo ASP.NET web API for OSRlib.NET
+
+This ASP.NET web API provides a sample implementation of a REST API that can serve [OSRlib.NET](https://github.com/mmacy/osrlib-dotnet) functionality to client applications.
+
+## Prerequisites
+
+- .NET SDK 7.0+
+
+## Getting started
+
+1. Clone this repository with `git clone`. Do _not_ switch into the repo directory after cloning.
+1. Clone the [OSRlib.NET](https://github.com/mmacy/osrlib-dotnet) repository.
+1. Move into the web API's `src` directory and start the API:
+
+    ```bash
+    cd osrlib-demo-api/src
+    dotnet run
+    ```
+
+1. View the Swagger interface for the API by navigating to `http://localhost:5207/swagger` in your browser.
+
+## How it works
+
+TBD
---
+++ src/Controllers/GameController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using osrlib.Dice;
+using osrlib.Core;
+
+namespace OSRlibDemoAPI.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class GameController : ControllerBase
+{
+    private readonly ILogger<GameController> _logger;
+
+    public GameController(ILogger<GameController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet()]
+    public int RollDice(int numDice, DieType numSides)
+    {
+        DiceHand dice = new DiceHand(numDice, numSides);
+
+        return DiceRoll.RollDice(dice);
+    }
+
+    [HttpGet()]
+    public Being RollCharacter(string name)
+    {
+        DiceHand d20 = new DiceHand(1, DieType.d20);
+        DiceRoll roll = new DiceRoll(d20);
+
+        Being character = new Being
+        {
+            Name = name,
+            Defense = roll.RollDice(),
+            MaxHitPoints = roll.RollDice() + 10
+        };
+        character.HitPoints = character.MaxHitPoints;
+        character.RollAbilities();
+
+        _logger.LogInformation("Rolled new character: {character}", character);
+
+        return character;
+    }
+}
